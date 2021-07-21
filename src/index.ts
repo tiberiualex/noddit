5022fcf692d876ab7c2a5de2e83f774171d9b455
tiberiualex <tiberiu.alexander@gmail.com>
2021-07-21 23:37:31 +0300

    Add db config

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from './entities/Post';
+import { dbDetails } from "./config";
 
 const main = async () => {
   const orm = await MikroORM.init({
     entities: [Post],
-    dbName: 'noddit',
-    user: '',
-    password: '',
-    debug: !__prod__,
+    dbName: 'test',
+    port: 5433,
+    user: dbDetails.user,
+    password: dbDetails.password,
+    debug: __prod__,
     type: 'postgresql'
   });
 
